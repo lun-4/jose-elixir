48d146d95fd8b5f3f3efc93870bde4c47078a810
Luna Mendes <lkmnds@gmail.com>
2017-09-10 20:22:21 -0300

    git pls

--- config/config.exs
+++
@@ -1,7 +0,0 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-use Mix.Config
-
-config :jose,
-  token: "MzE5NTIyNzkyODU0OTEzMDI1.DJdYEA.UEPkg4nv9eiNcsj30mEYExMJCpw",
-  prefix: "ej!"
