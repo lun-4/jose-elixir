14511f069141f1a87f938d1980fbe445273c5501
Luna Mendes <lkmnds@gmail.com>
2017-09-21 20:20:58 -0300

    basic: give arity

--- lib/ext/basic.ex
+++ lib/ext/basic.ex
@@ -5,6 +5,9 @@ defmodule Basic do
   defmodule Commands do
     use Alchemy.Cogs
 
+    @doc """
+    Show help about all commands in José.
+    """
     Cogs.def help do
       e = %Alchemy.Embed{description: ""}
       |> Embed.title("Command list")
@@ -14,7 +17,15 @@ defmodule Basic do
       reduced = all
       |> Map.keys
       |> Enum.map_reduce(e, fn (key, e) ->
-        {key, Embed.description(e, "#{e.description}\n#{key}")}
+        value = Map.get(all, key)
+
+        module = elem value, 0
+        arity = elem value, 1
+        name = elem value, 2
+
+        formatted = "#{name}/#{arity}"
+        new_embed = Embed.description(e, "#{e.description}\n#{formatted}")
+        {formatted, new_embed)}
       end)
 
       help_embed = elem reduced, 1
