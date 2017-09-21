20969aebfe38d9b57f172d8a2fb2247f87c08299
Luna Mendes <lkmnds@gmail.com>
2017-09-21 00:15:52 -0300

    use latest alchemy from git

--- mix.exs
+++ mix.exs
@@ -22,9 +22,7 @@ defmodule Jose.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
-      {:alchemy, "~> 0.6.0", hex: :discord_alchemy},
+      {:alchemy, "~> 0.6.0", github: "cronokirby/alchemy"},
       {:jcoin, "~> 0.1.0", github: "lnmds/jcoin"}
     ]
   end
