47c4379762da66037d1b741be4badd0aac1191f6
Luna Mendes <lkmnds@gmail.com>
2017-11-13 19:53:41 -0300

    basic: add vmstats cmd

--- lib/ext/basic.ex
+++ lib/ext/basic.ex
@@ -5,6 +5,8 @@ defmodule Basic do
   defmodule Commands do
     use Alchemy.Cogs
 
+    @red_embed %Alchemy.Embed{color: 0x660099}
+
     @doc """
     Show help about all commands in José.
     """
@@ -19,7 +21,7 @@ defmodule Basic do
       |> Enum.map_reduce(e, fn (key, e) ->
         value = Map.get(all, key)
 
-        module = elem value, 0
+        #module = elem value, 0
         arity = elem value, 1
         name = elem value, 2
 
@@ -33,5 +35,33 @@ defmodule Basic do
 
     end
 
+    @doc """
+    Erlang VM stats.
+    """
+    Cogs.def vmstats do
+      memories = :erlang.memory()
+      processes = length :erlang.processes()
+      {{_, io_input}, {_, io_output}}  = :erlang.statistics(:io)
+      
+      mem_format = fn
+	mem, :kb -> "#{div(mem, 1000)} KB"
+	mem, :mb -> "#{div(mem, 1_000_000)} MB"
+      end
+
+      info = [
+	{"Processes", "#{processes}"},
+	{"Total Memory", mem_format.(memories[:total], :mb)},
+	{"IO Input", mem_format.(io_input, :mb)},
+	{"Process Memory", mem_format.(memories[:processes], :mb)},
+	{"Code Memory", mem_format.(memories[:code], :mb)},
+	{"IO Output", mem_format.(io_output, :mb)},
+	{"ETS Memory", mem_format.(memories[:ets], :kb)},
+	{"Atom Memory", mem_format.(memories[:atom], :kb)}
+      ]
+      Enum.reduce(info, @red_embed, fn {name, value}, embed ->
+	Alchemy.Embed.field(embed, name, value, inline: true)
+      end)
+      |> Embed.send
+    end
   end
 end
